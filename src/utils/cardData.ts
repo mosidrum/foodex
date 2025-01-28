ccbe40f89a0d7b4d49c71fbad81dd5b83ddfe632
mosidrum <mosiokanga@gmail.com>
2025-01-28 15:12:04 +0100

    clean up files

--- src/utils/cardData.ts
+++ src/utils/cardData.ts
@@ -2,7 +2,6 @@ import { CardProps } from "../components/Card";
 import food1 from "../assets/food1.png";
 import food2 from "../assets/food2.png";
 import food3 from "../assets/food3.png";
-import food4 from "../assets/food4.png";
 
 export const cardData: CardProps[] = [
   {
